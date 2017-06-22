d178ca4a3804d915c22487c58112685acfc7b165
zaxlct <zaxlct@foxmail.com>
2017-06-22 10:02:28 +0800

    add wxParse

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,10 +1,20 @@
+const WxParse = require('../../common/wxParse/wxParse.js');
+console.log(WxParse)
 Page({
   data:{
-    text:"Page detail"
+    text:"Page detail",
+    wxParseData: '',
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
     console.log(options)
+    var html = '<div class="content mousetrap"><h1 id="wxparse">wxParse-微信小程序富文本解析自定义组件，支持HTML及markdown解析</h1> <blockquote><p>微信小程序富文本解析自定义组件，支持HTML及markdown解析</p></blockquote> <h2 id="-">功能列表</h2> <ul> <li>动态加载代码</li> <li>html转json</li> <li>markdown转html转json</li> <li>富文本markdown解析</li> <li>自定义层级解析</li> <li>自定义样式表</li> </ul> <h2 id="-">文件作用</h2> <pre class="hljs scala"><code class="scala"><span class="hljs-comment">// wxParse.wxml</span> <span class="hljs-comment">//用于解析使用的模版</span> .css{ text-align:center; } &lt;div&gt;test div&lt;/div&gt;</code></pre> <h2 id="-">开发信息</h2> <p><a href="http://weappdev.com" target="_blank">微信小程序开发论坛</a></p> <div class="image-package"><img src="http://weappdev.com/uploads/default/original/1X/9156b32bd04323f35d0957796f126b8a54595c97.png" data-original-src="http://weappdev.com/uploads/default/original/1X/9156b32bd04323f35d0957796f126b8a54595c97.png"><br><div class="image-caption">微信小程序logo</div></div> </div>';
+
+    const that = this
+    //更新数据
+    that.setData({
+      wxParseData: WxParse.wxParse('article', 'html', html, that)
+    })
   },
   onReady:function(){
     // 页面渲染完成
