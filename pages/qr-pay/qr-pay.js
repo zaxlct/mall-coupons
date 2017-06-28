e6d8a81959772268b86ff02da298cd307a224e0d
zaxlct <zaxlct@foxmail.com>
2017-06-28 23:21:38 +0800

    :sleepy:

--- pages/qr-pay/qr-pay.js
+++ pages/qr-pay/qr-pay.js
@@ -1,20 +1,73 @@
+const app = getApp()
 Page({
   data:{
-    text:"Page qr-pay"
+    expenditure: 0,
+    merchant_id: 1,
+    merchant_info: {},
+    coupons: [],
+    seletedCouponNum: 0,
   },
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  onLoad(query){
+    const { merchant_id = 1 } = query
+    this.setData({
+      merchant_id
+    })
+    const self = this
+    wx.getStorage({
+      key: 'token',
+      success(res) {
+        self.fetchData(res.data)
+      } 
+    })
   },
-  onReady:function(){
-    // 页面渲染完成
+  fetchData(token) {
+    const self = this
+    wx.request({
+      url: 'https://gjb.demo.chilunyc.com/api/weapp/merchants/' + self.data.merchant_id,
+      header: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json'
+      },
+      success(res) {
+        const { data } = res
+        if(data.data) {
+          const result = data.data
+          let { coupons = [] } = result
+          coupons.forEach(item => item.isSeleted = false)
+          self.setData({
+            merchant_info: {
+              logo: result.logo,
+              name: result.name,
+            },
+            coupons,
+          })
+        } else if(data.errors) {
+          if(data.errors.status == 401) {
+            app.wxLogin(self.fetchData)
+          }
+        }
+      },
+      fail() {
+        wx.showToas({title: '网络错误，请重试！'})
+      },
+    })
   },
-  onShow:function(){
-    // 页面显示
+  bindExpenditure(e) {
+    this.setData({
+      expenditure: e.detail.value.trim()
+    })
   },
-  onHide:function(){
-    // 页面隐藏
+  checkboxChange(e) {
+    let { coupons } = this.data
+    coupons.forEach(item => item.isSeleted = false)
+    e.detail.value.forEach(index => {
+      coupons[Number(index)].isSeleted = true
+    })
+    this.setData({
+      coupons
+    })
   },
-  onUnload:function(){
+  onUnload(){
     // 页面关闭
   }
 })
