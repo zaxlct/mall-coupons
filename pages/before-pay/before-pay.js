dcef2d47eccebca598b9b222ff18427a67d12e76
zaxlct <zaxlct@foxmail.com>
2017-06-28 17:14:40 +0800

    :watermelon:

--- pages/before-pay/before-pay.js
+++ pages/before-pay/before-pay.js
@@ -1,20 +1,190 @@
+const app = getApp()
+let timer = null
+
 Page({
   data:{
-    text:"Page before-pay"
+    name: '',
+    price: 0,
+    num: 1,
+    isBindPhone: false,
+    isSendMsging: false,
+    phone: '',
+    code: '',
+    nickname: '',
+    timeOut: 60,
   },
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  onLoad(options){
+    const { name = '', price = 0 } = options
+    const self = this
+    this.setData({
+      name,
+      price: Number(price),
+    })
+    wx.getStorage({
+      key: 'token',
+      success(res) {
+        self.fetchIsBindPhone(res.data)
+      } 
+    })
   },
-  onReady:function(){
-    // 页面渲染完成
+
+  fetchIsBindPhone(token) {
+    const self = this
+    wx.request({
+      url: 'https://gjb.demo.chilunyc.com/api/weapp/users/check',
+      header: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      success(res) {
+        const { data } = res
+        console.log(data)
+        if(data.data) {
+          self.setData({
+            isBindPhone: Boolean(Number(data.data.status)),
+          })
+        } else if(data.errors) {
+          if(data.errors.status == 401) {
+            app.wxLogin(self.fetchData)
+          }
+        }
+      },
+      fail() {
+        wx.showToas({title: '网络错误，请重试！'})
+      },
+    })
   },
-  onShow:function(){
-    // 页面显示
+
+  subtractNum() {
+    if(this.data.num > 1) {
+      this.setData({
+        num: this.data.num - 1
+      })
+    }
   },
-  onHide:function(){
-    // 页面隐藏
+
+  addNum() {
+    this.setData({
+      num: this.data.num + 1
+    })
   },
-  onUnload:function(){
-    // 页面关闭
+
+  bindPhone(e) {
+    this.setData({
+      phone: e.detail.value.trim()
+    })
+  },
+
+  bindCode(e) {
+    this.setData({
+      code: e.detail.value.trim()
+    })
+  },
+
+  sendMsg() {
+    if(!this.data.phone) {
+      wx.showModal({
+        title: '请输入手机号！',
+        content: '',
+        showCancel: false,
+      })
+      return 
+    }
+
+    const self = this
+    this.setData({
+      isSendMsging: true,
+    })
+
+    // 获取用户昵称
+    wx.getUserInfo({
+      success(res) {
+        self.setData({
+          nickname:  res.userInfo.nickName,
+        })
+      }
+    })
+
+    // 60s 倒计时
+    timer = setInterval(() => {
+      if(this.data.timeOut === 1) {
+        clearInterval(timer)
+        this.setData({
+          isSendMsging: false,
+          timeOut: 60,
+        })
+        return 
+      }
+      this.setData({
+        timeOut: this.data.timeOut - 1
+      })
+    }, 1000)
+
+    // 调用发送短信
+    wx.getStorage({
+      key: 'token',
+      success(res) {
+        self.fetchSendMsg(res.data)
+      } 
+    })
+  },
+
+  // 发送短信
+  fetchSendMsg(token) {
+    const self = this
+    wx.request({
+      url: 'https://gjb.demo.chilunyc.com/api/weapp/sms',
+      header: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json'
+      },
+      data: {phone: self.data.phone},
+      method: 'POST',
+      success(res) {
+        const { data } = res
+        console.log(data)
+        if(data.data) {
+          if(data.data.status != 1) {
+            wx.showModal({
+              title: '手机号发送失败，请退出重试！',
+              content: '',
+              showCancel: false,
+            })
+          }
+        } else if(data.errors) {
+          if(data.errors.status == 401) {
+            app.wxLogin(self.fetchData)
+          }
+        }
+      },
+      fail() {
+        wx.showToas({title: '网络错误，请重试！'})
+      },
+    })
+  },
+
+  submitPay() {
+    if(!this.data.phone) {
+      wx.showModal({
+        title: '请输入手机号！',
+        content: '',
+        showCancel: false,
+      })
+      return 
+    }
+
+    if(!this.data.code) {
+      wx.showModal({
+        title: '请输入验证码！',
+        content: '',
+        showCancel: false,
+      })
+      return 
+    }
+  },
+
+  onUnload(){
+    if(timer) clearInterval(timer)
   }
 })
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,30 +1,52 @@
 const WxParse = require('../../common/wxParse/wxParse.js');
+const app = getApp()
 Page({
   data:{
-    text:"Page detail",
     wxParseData: '',
+    id: '',
+    order_id: '',
+    item: {},
   },
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-    console.log(options)
-    var html = '<h1>&nbsp;　　湖南的糖油粑粑好吃啊，在自家厨房里也能做得出来哦!这次小编推荐的是一道DIY粑粑制作法。只需要简单两步，玉米糍粑就变成了长沙街头的地道热卖小吃。</h1><p><img alt="图片替换文字" src="http://static.chinacaipu.com/d/file/menu/hunanmeishi/2015-01-13/e9f6d6b9dc3234a0b1d27d483c8439ad.jpg" style="height:300px; width:400px" /></p><p><strong>材料：</strong><a href="http://baidu.com">玉米糍粑</a></p><p><strong>调料：</strong><em>红糖</em>，<s>白糖</s>，蜂蜜</p><ol>	<li>第一步是这样子的。</li>	<li>第二步</li>	<li>第三步。</li></ol><p><img alt="" src="http://static.chinacaipu.com/d/file/menu/hunanmeishi/2015-01-13/77f554ab56925b7fe4fd0997759c7e67.jpg" style="height:372px; width:440px" /></p><ul>	<li>因为糍粑不太吸油，所以煎的时候多出来的油倒出来比较好</li>	<li>红糖的份量不用太多，太多上色就比较深了。</li></ul><p><img alt="" src="http://static.chinacaipu.com/d/file/menu/hunanmeishi/2015-01-13/a14b462e8bc11685403237786c9056a1.jpg" style="height:224px; width:400px" /></p><p>&nbsp;</p><blockquote><p>当嫩玉米散发出甜丝丝的香味，美味诱惑实在是无法抵挡，大口啃玉米特别过瘾，但你有注意到啃过的玉米棒上总是留下许多淡黄色的嫩小颗粒吗?这些被浪费掉的小黄颗粒却是最有营养的玉米精华，以后要小口啃玉米，不然就亏大了!</p></blockquote>';
-
-    const that = this
-    //更新数据
-    that.setData({
-      wxParseData: WxParse.wxParse('article', 'html', html, that)
+  onLoad(options){
+    const { id = '', order_id = '' } = options
+    const self = this
+    this.setData({
+      id,
+      order_id,
+    })
+    
+    wx.getStorage({
+      key: 'token',
+      success(res) {
+        self.fetchCouponDetail(res.data)
+      } 
     })
   },
-  onReady:function(){
-    // 页面渲染完成
+  fetchCouponDetail(token) {
+    const self = this
+    wx.request({
+      url: `https://gjb.demo.chilunyc.com/api/weapp/coupons/${self.data.id}?order_id=${self.data.order_id}`,
+      header: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json'
+      },
+      success(res) {
+        const { data } = res
+        console.log(data)
+        if(data.data) {
+          self.setData({
+            item: data.data,
+            wxParseData: WxParse.wxParse('article', 'html', data.data.details, self)
+          })
+        } else if(data.errors) {
+          if(data.errors.status == 401) {
+            app.wxLogin(self.fetchData)
+          }
+        }
+      },
+      fail() {
+        wx.showToas({title: '网络错误，请重试！'})
+      },
+    })
   },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
 })
