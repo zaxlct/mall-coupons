6552638c9f4c28a73b5ba7810d69c24c45ed889c
zaxlct <zaxlct@foxmail.com>
2017-06-29 10:25:52 +0800

    Coupon discount amount calculation

--- pages/qr-pay/qr-pay.js
+++ pages/qr-pay/qr-pay.js
@@ -1,11 +1,12 @@
 const app = getApp()
 Page({
   data:{
-    expenditure: 0,
-    merchant_id: 1,
+    consume: 0, // 用户输入的消费额度
+    merchant_id: 1, // 商家 id
     merchant_info: {},
-    coupons: [],
-    seletedCouponNum: 0,
+    coupons: [], // 优惠券列表
+    couponTotal: 0, // 优惠券一共抵用了多少钱
+    pay_sum: 0, // 需要支付的金额
   },
   onLoad(query){
     const { merchant_id = 1 } = query
@@ -33,6 +34,7 @@ Page({
         if(data.data) {
           const result = data.data
           let { coupons = [] } = result
+          // TODO 再 forEach 一个 disabled: false 字段
           coupons.forEach(item => item.isSeleted = false)
           self.setData({
             merchant_info: {
@@ -52,21 +54,38 @@ Page({
       },
     })
   },
-  bindExpenditure(e) {
+  bindConsume(e) {
+    // TODO 用户改变输入的消费额度时，要判断消费额度是否大于当前已选择的优惠券总额
     this.setData({
-      expenditure: e.detail.value.trim()
+      consume: Number(e.detail.value.trim())
     })
   },
   checkboxChange(e) {
+    // TODO 防止出现用户多用了优惠券的情况
     let { coupons } = this.data
     coupons.forEach(item => item.isSeleted = false)
     e.detail.value.forEach(index => {
       coupons[Number(index)].isSeleted = true
     })
+    const couponTotal = coupons.reduce((total,prev) => total + prev.face_value * Number(prev.isSeleted), 0)
+    const pay_sum = this.data.consume - couponTotal < 0 ? 0 : this.data.consume - couponTotal
+
+    //TODO 用户选择的优惠券总额大于 用户输入的消费额度 时，禁止用户再添加优惠券
+    // if(couponTotal >= this.data.consume) {
+    //   coupons.forEach(item => item.disabled = true)
+    // }
+
     this.setData({
-      coupons
+      coupons,
+      couponTotal,
+      pay_sum,
     })
   },
+
+  submitPay() {
+    // TODO consume 不能为空
+  },
+
   onUnload(){
     // 页面关闭
   }
