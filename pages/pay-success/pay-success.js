3564d7b8c595a48effa37bd3f5c59aa2658156b2
zaxlct <zaxlct@foxmail.com>
2017-07-13 21:24:22 +0800

    Pay-success

---
+++ pages/pay-success/pay-success.js
@@ -0,0 +1,31 @@
+Page({
+  data:{
+    nonce_str: '',
+    consume: '',
+    couponTotal: '',
+    pay_sum: '',
+  },
+  onLoad(options){
+    console.log('pay-success-options', options)
+    const {
+      nonce_str = '',
+      consume = 0,
+      couponTotal = 0,
+      pay_sum = 0,
+    } = options
+
+    this.setData({
+      nonce_str,
+      consume,
+      couponTotal,
+      pay_sum,
+    })
+  },
+
+  linkIndexPage() {
+    // TODO 如果这个用户，没用优惠券，直接付款呢？
+    wx.navigateTo({
+      url: '/pages/index/index'
+    })
+  },
+})
