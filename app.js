fb1869c105bdcd930346434119da38894a016045
zaxlct <zaxlct@foxmail.com>
2017-07-23 14:59:54 +0800

    重构：Token 类来管理token

--- app.js
+++ app.js
@@ -1,49 +1,13 @@
-//app.js
+import Token from 'utils/token'
+
 App({
   onLaunch() {
-    const self = this
-    wx.checkSession({
-      success() {
-        console.log('检测已登录！')
-      },
-      fail() {
-        self.wxLogin()
-      }
-    })
-  },
-
-  /* TODO
-   * 1. 新的微信号测试
-   * 2. Token 过期，code 未过期的情况
-   */
-  wxLogin(cb) {
-    const self = this
-    wx.login({  
-      success(res_code) {
-        if (res_code.code) {
-          wx.request({
-            url: 'https://gjb.demo.chilunyc.com/api/weapp/users/login',
-            method: 'POST',
-            data: {
-              code: res_code.code
-            },
-            success(res) {
-              wx.setStorage({
-                key: "token",
-                data: res.data.data.token,
-                success() {
-                  typeof cb == "function" && cb(res.data.data.token)
-                }
-              })
-            },
-            fail() {
-              wx.showToast({title: '网络错误，请重新授权！'})
-            },
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      }
-    })
-  },
+    /*  
+     * token 过期速度远远快于微信的 session（用户登录态），
+     * 获取 token 会更新微信的 session，故这里不需要执行 wx.checkSession 
+     */
+    
+    const token = new Token()
+    token.verify()
+  }
 })
---
+++ utils/token.js
@@ -0,0 +1,67 @@
+import Config from './config'
+
+export default class Token {
+  constructor() {
+    this.verifyUrl = Config.baseUrl + 'token/verify'
+    this.tokenUrl = Config.baseUrl + 'users/login'
+  }
+
+  verify() {
+    const self = this
+    wx.getStorage({
+      key: 'token',
+      success(token) {
+        self._verifyFromServer(token)
+      },
+      fail() {
+        self.getTokenFromServer()
+      }
+    })
+  }
+
+  //验证 token 是否过期
+  _verifyFromServer(token) {
+    const self = this
+    wx.request({
+      url: self.verifyUrl,
+      header: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json'
+      },
+      success(res){
+        if(res.statusCode == 401) {
+          self.getTokenFromServer()
+        }
+      },
+      fail() {
+      },
+    })
+  }
+
+  //获取新的 token
+  getTokenFromServer(callBack) {
+    const self = this
+    wx.login({  
+      success(res_code) {
+        if (res_code.code) {
+          wx.request({
+            url: self.tokenUrl,
+            method: 'POST',
+            data: {
+              code: res_code.code
+            },
+            success(res) {
+              wx.setStorageSync('token', res.data.data.token)
+              typeof callBack == 'function' && callBack(res.data.data.token)
+            },
+            fail() {
+              wx.showToast({title: '网络错误，请重新授权！'})
+            },
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    })
+  }
+}
